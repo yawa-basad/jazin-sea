7b538bd3bd405cb4eea3076c6d1a077fd82f9fc6
nr <99275945+giterhaber@users.noreply.github.com>
2024-06-29 21:38:38 +0000

    yawa

--- app/assets/[id]/layout.tsx
+++ app/assets/[id]/layout.tsx
@@ -71,15 +71,23 @@ export async function generateMetadata(
  
 
 
-export default function RootLayout({
-    children,
-  }: Readonly<{
-    children: React.ReactNode;
-  }>) {
-    return (
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
-      </html>
-    );
-  }
+// export default function RootLayout({
+//     children,
+//   }: Readonly<{
+//     children: React.ReactNode;
+//   }>) {
+//     return (
+//       <html lang="en">
+//         <body className={inter.className}>{children}</body>
+//       </html>
+//     );
+//   }
   
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
